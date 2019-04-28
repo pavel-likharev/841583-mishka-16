aa23eae3644e055ce2cb8991b5b7b26e8dfa5ea2
PavelLikharev <pavel7321@mail.ru>
2019-04-29 03:08:27 +0700

    Выполнил ретинизацию и переработал стилизацию навигации

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,13 @@
-if(window.innerWidth < 768) {
-  var buttonNav = document.querySelector(".header__button-main-nav");
-  var menu = document.querySelector(".navigation");
+var buttonNav = document.querySelector(".header__button-call-nav");
+var menu = document.querySelector(".navigation");
 
-  window.onload = function() {
-    menu.classList.add("navigation--close");
-    buttonNav.classList.add("header__button-main-nav--open");
-  };
+window.onload = function() {
+  menu.classList.add("navigation--close");
+  buttonNav.classList.add("header__button-call-nav--open");
+};
 
-  buttonNav.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    menu.classList.toggle("navigation--open");
-    buttonNav.classList.toggle("header__button-main-nav--close");
-  });
-}
+buttonNav.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  menu.classList.toggle("navigation--open");
+  buttonNav.classList.toggle("header__button-call-nav--close");
+});
